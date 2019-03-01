b4312ca5b633c5f08babf17f3af6082687227d9a
github username <github email address>
2019-03-01 14:40:09 -0500

    done

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,27 @@
+
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each{|k, v|
+    v.invert.each {|names, info|
+      names.each  {|x|
+        pigeon_list.store(x, {})
+      }
+    }
+  }
+  pigeon_list.each {|name, keys|
+    data.each {|k, v|
+      pigeon_list[name].store(k, [])
+      }
+    }
+  pigeon_list.each {|name, keys|
+    data.each {|k, v|
+      v.select{|key, values|
+          if values.include?(name)
+            pigeon_list[name][k]<< key.to_s
+          end
+      }
+    }
+  }
+    return pigeon_list
 end
